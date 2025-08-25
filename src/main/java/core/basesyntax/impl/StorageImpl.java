6ca4f7398099ee7bb0e549c661af629583dd5085
anton-chornobai <antonchornobajj1@gmail.com>
2025-08-25 20:03:09 +0200

    done

--- src/main/java/core/basesyntax/impl/StorageImpl.java
+++ src/main/java/core/basesyntax/impl/StorageImpl.java
@@ -1,54 +1,58 @@
 package core.basesyntax.impl;
 
+import java.util.Objects;
+
 import core.basesyntax.Storage;
 
 public class StorageImpl<K, V> implements Storage<K, V> {
-    static final int MAX_ARR_LENGTH = 10;
-    K[] keys;
-    V[] values;
-    int count;
+    private static final int CAPACITY = 10;
+
+    public static final class Entry<K, V> {
+        private final K key;
+        private V value;
+
+        public Entry(K key, V value) {
+            this.key = key;
+            this.value = value;
+        }
+    }
 
     @SuppressWarnings("unchecked")
-    public StorageImpl() {
-        keys = (K[]) new Object[MAX_ARR_LENGTH];
-        values = (V[]) new Object[MAX_ARR_LENGTH];
-        count = 0;
+    private final Entry<K, V>[] array = (Entry<K, V>[]) new Entry[CAPACITY];
+    private int size = 0;
+
+    public boolean keysEqual(K a, K b) {
+        return Objects.equals(a, b);
     }
 
     @Override
     public void put(K key, V value) {
-        for (int i = 0; i < MAX_ARR_LENGTH; i++) {
-            if (keyEquals(keys[i], key)) {
-                values[i] = value;
+        for (int i = 0; i < size; i++) {
+            if (keysEqual(array[i].key, key)) {
+                array[i].value = value;
                 return;
             }
         }
-        if (count < MAX_ARR_LENGTH) {
-            keys[count] = key;
-            values[count] = value;
-            count++;
-        } else {
-            throw new IllegalStateException("Storage is full");
+
+        if (size >= CAPACITY) {
+            throw new RuntimeException("The storage is full.");
         }
+
+        array[size++] = new Entry<>(key, value);
     }
 
     @Override
     public V get(K key) {
-        for (int i = 0; i < MAX_ARR_LENGTH; i++) {
-            if (keyEquals(key, keys[i])) {
-                return values[i];
+        for (int i = 0; i < size; i++) {
+            if (keysEqual(array[i].key, key)) {
+                return array[i].value;
             }
         }
-
         return null;
-    }
-
-    private boolean keyEquals(K a, K b) {
-        return (a == b) || (a != null && a.equals(b));
     }
 
     @Override
     public int size() {
-        return count;
+        return size;
     }
 }
